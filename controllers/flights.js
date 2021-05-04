af72162c1a5966b72d56dcd64cf193b20d2d95b8
Jasnoor Nagpal <jasnoornagpal@Jasnoors-MBP.ht.home>
2021-05-03 20:55:13 -0400

    create done

--- controllers/flights.js
+++ controllers/flights.js
@@ -4,7 +4,7 @@ const Flight = require('../models/flight');
 module.exports = {
     index,
     new: newFlight,
-    create
+    create: createFlight
 };
 
 function index(req, res) {
@@ -17,13 +17,19 @@ function newFlight(req, res) {
     res.render('flights/new')
 }
 
-function create(req, res) {
+async function createFlight(req, res) {
+    Flight.create({
+        airline: req.body.airline,
+        airport: req.body.airport,
+        flightNo: req.body.flightNo,
+        departs: req.body.departs
+    });
     const flight = new Flight(req.body);
     flight.save(function(err) {
     // one way to handle errors
     if (err) return res.render('flights/new');
     console.log(flight);
     // for now, redirect right back to new.ejs
-    res.redirect('/flights/new');
+    res.redirect('/flights');
   });
 }
